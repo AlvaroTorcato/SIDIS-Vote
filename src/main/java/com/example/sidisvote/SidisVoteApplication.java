384988ca22caf048cf34c4fb9c23aa587e4d395b
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-10-19 09:40:13 +0100

    Initial commit

---
+++ src/main/java/com/example/sidisvote/SidisVoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sidisvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SidisVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SidisVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sidisvote/SidisVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sidisvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SidisVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
