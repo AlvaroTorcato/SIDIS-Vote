a4b4e1c44e27f9e0b2e7caa7b94ac26f1d9044d8
Tiago Nora <tiagonora2002@gmail.com>
2022-11-06 02:09:40 +0000

    Update

---
+++ src/main/java/com/example/sidisvote/service/RequestService.java
@@ -0,0 +1,63 @@
+package com.example.sidisvote.service;
+
+import com.example.sidisvote.model.UserDetailsDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+@Service
+public class RequestService {
+    public String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7, headerAuth.length());
+        }
+
+        return null;
+    }
+
+    public UserDetailsDTO makeRequestToAutentication(String jwt){
+        String urlRequest = "http://localhost:8084/auth/search" + jwt;
+        UserDetailsDTO user = null;
+        try {
+            InputStream responseStream = openConn(urlRequest).getInputStream();
+
+            ObjectMapper mapper = new ObjectMapper();
+
+            user = mapper.readValue(responseStream, UserDetailsDTO.class);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        return user;
+    }
+    private HttpURLConnection openConn(String baseUrl) throws IOException {
+
+        URL url = new URL(baseUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestProperty("accept", "application/json");
+
+        return connection;
+    }
+    public int getStatusCodeOfReview(int reviewId){
+        int statusCode;
+        try{
+            String urlRequest = "http://localhost:8082/reviews/search/" + reviewId;
+            URL url = new URL(urlRequest);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            statusCode = connection.getResponseCode();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return statusCode;
+    }
+}
